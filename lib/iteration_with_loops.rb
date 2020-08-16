15c533579b46a4536ae99cebc4923292a8fb9cf4
Nicholas Baccete <nickbaccete@gmail.com>
2020-08-16 19:56:47 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
     if src[row_index][element_index].even?
-      p srx[row_index][element_index]
+      p src[row_index][element_index]
     end
     element_index += 1
   end
